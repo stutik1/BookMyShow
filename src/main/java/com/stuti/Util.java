1094bc2287e60460c738b8168b30eaf41e401cea
megha <stutimegha2019@gmail.com>
2024-02-17 18:30:51 +0530

    validation

--- src/main/java/com/stuti/Util.java
+++ src/main/java/com/stuti/Util.java
@@ -1,2 +1,25 @@
-package com.stuti;public class util {
+package com.stuti;
+
+public class Util {
+
+    public static boolean validateEmail(String email) {
+        if (email == null || email.isEmpty()) {
+            return false;
+        }
+
+//        int atIndex = email.indexOf('@');
+//        int dotIndex = email.lastIndexOf('.');
+//
+//        // Check if '@' and '.' are present and in correct positions
+//        if (atIndex <= 0 || dotIndex <= atIndex || dotIndex == email.length() - 1) {
+//            return false;
+//        }
+//        return true;
+
+        if (!email.contains("@") || !email.contains(".")) {
+            return false;
+        }
+        return true;
+
+    }
 }
